2d0ab7726046320c54d6c678a833251565854460
Kristian W Del Rio <kristian.delrio@gmail.com>
2020-05-25 17:06:02 -0400

    Update gruntfile.js
    re-installed grunt globally on computer

--- gruntfile.js
+++ gruntfile.js
@@ -3,19 +3,19 @@ module.exprts = function(grunt){
   // Configure the various tasks and plugins that we'll be using
 
   grunt.initConfig({
-    
+    //configure
   }); // END initConfig
 
 
-  //function Loads your task to the Node Package Manager (NPM)
+  //function Loads plugins
   
-  // grunt.loadNpmTasks('');
+  // grunt.loadNpmTasks('')
 
 
   /* function Publishes your task to the Node Package Manager (NPM) node_module folder in your project directory. Then it can be ran in the command prompt */
 
-  grunt.registerTask('run',function(){
-      console.log('Hello, World');
+  grunt.registerTask('run', function(){
+      console.log('Hello, World.');
   });
 
 }; // grunt Wrapper function
