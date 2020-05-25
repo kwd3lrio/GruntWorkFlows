43eb27c81ac541a83c082865e15f24401bb2445f
Kristian W Del Rio <kristian.delrio@gmail.com>
2020-05-25 14:00:24 -0400

    Add package.json file and installed grunt cli tools

---
+++ gruntfile.js
@@ -0,0 +1,32 @@
+module.exprts = function(grunt){
+  
+  // Configure the various tasks and plugins that we'll be using
+  grunt.initConfig({
+    
+    concat : {
+      dist : {
+        src: ['components/scripts/*.js'],
+        dest: 'builds/development/js/script.js'            
+      }
+    },  
+    
+    'uglify': { 
+      'development': { 
+        'files': {
+          'build/app.min.js': ['src/app.js', 'src/lib.js']
+        } 
+      } //END Development
+    } // END Uglify task code
+
+  }); //initConfig
+
+  
+  //function Loads your task to the Node Package Manager (NPM)
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  //function Publishes your task to the Node Package Manager (NPM)
+  /*This allows grunt to run the task on your Command Prompt app with just the "grunt" command.no need to type the task names */
+  grunt.registerTask('default', ['grunt-contrib-concat','uglify']);
+
+}; // grunt Wrapper function
