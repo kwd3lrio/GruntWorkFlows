2032c54950de791bbc75ae6931b022b7ae3d6bd4
Kristian W Del Rio <kristian.delrio@gmail.com>
2020-05-25 16:46:43 -0400

    Update gruntfile.js
    added only one task to gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,35 +1,21 @@
 module.exprts = function(grunt){
   
   // Configure the various tasks and plugins that we'll be using
+
   grunt.initConfig({
     
-    concat : {
-      dist : {
-        src: ['components/scripts/*.js'],
-        dest: 'builds/development/js/script.js'            
-      }
-    },  
-    
-    'uglify': { 
-      'development': { 
-        'files': {
-          'build/app.min.js': ['src/app.js', 'src/lib.js']
-        } 
-      } //END Development
-    } // END Uglify task code
-
-  }); //initConfig
+  }); // END initConfig
 
 
   //function Loads your task to the Node Package Manager (NPM)
   
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('');
 
 
-  //function Publishes your task to the Node Package Manager (NPM)
-  /*This allows grunt to run the task on your Command Prompt app with just the "grunt" command.no need to type the task names */
+  /* function Publishes your task to the Node Package Manager (NPM) node_module folder in your project directory. Then it can be ran in the command prompt */
 
-  grunt.registerTask('default', ['grunt-contrib-concat','uglify']);
+  grunt.registerTask('run',function(){
+      console.log('Hello, World');
+  });
 
 }; // grunt Wrapper function
