ece6995d7034978ed7314aeff5959bc355e4bd17
Kristian W Del Rio <kristian.delrio@gmail.com>
2020-05-25 18:43:40 -0400

    Ran concat task to add concatinated js script and css styles

--- gruntfile.js
+++ gruntfile.js
@@ -7,6 +7,10 @@ module.exports = function(grunt){
       js: {
         src : ['js/*.js'],
         dest : 'build/scripts.js' 
+      },
+      css : {
+        src : ['css/*.css'],
+        dest : 'build/styles.css' 
       }  
     } //END Concat plugin task
   }); // END initConfig
@@ -19,6 +23,11 @@ module.exports = function(grunt){
 
   /* function Publishes your task to the Node Package Manager (NPM) node_module folder in your project directory. Then it can be ran in the command prompt */
 
+  //run concat tasks individually in command line
+  grunt.registerTask('concat-js', ['concat:js']);
+  grunt.registerTask('concat-css', ['concat:css']);
+  
+  // Test register task
   /* grunt.registerTask('run', function(){
       console.log('Hello, World.');
   }); */
