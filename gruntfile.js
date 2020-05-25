018db7e3f9cb1248c271ce1ee27198a451e9e2ef
Kristian W Del Rio <kristian.delrio@gmail.com>
2020-05-25 18:21:17 -0400

    Added js and css directories. Update gruntfile with concat plugin
    Add js and css files from Shaun Pendill github tutoria

--- gruntfile.js
+++ gruntfile.js
@@ -3,18 +3,23 @@ module.exports = function(grunt){
   // Configure the various tasks and plugins that we'll be using
 
   grunt.initConfig({
-    //configure
+    concat : {
+      js: {
+        src : ['js/*.js'],
+        dest : 'build/scripts.js' 
+      }  
+    } //END Concat plugin task
   }); // END initConfig
 
 
   //function Loads plugins
   
-  // grunt.loadNpmTasks('')
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
 
   /* function Publishes your task to the Node Package Manager (NPM) node_module folder in your project directory. Then it can be ran in the command prompt */
 
-  grunt.registerTask('run', function(){
+  //grunt.registerTask('run', function(){
       console.log('Hello, World.');
   });
 
