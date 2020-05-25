98b7475d88df0c97906ae374ba6591da5bc26898
Kristian W Del Rio <kristian.delrio@gmail.com>
2020-05-25 18:23:27 -0400

    block quote error at line 22 thru 24 of gruntfile js FIXED

--- gruntfile.js
+++ gruntfile.js
@@ -19,8 +19,8 @@ module.exports = function(grunt){
 
   /* function Publishes your task to the Node Package Manager (NPM) node_module folder in your project directory. Then it can be ran in the command prompt */
 
-  //grunt.registerTask('run', function(){
+  /* grunt.registerTask('run', function(){
       console.log('Hello, World.');
-  });
+  }); */
 
 }; // grunt Wrapper function
